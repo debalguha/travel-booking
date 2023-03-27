719e4b7c5aeb014407607853210f4b49aba72d70
debalguha <debalguha.active@gmail.com>
2023-03-27 16:48:05 +1100

    Spring boot package

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker rmi $(docker image ls|grep 'sb.*sv' |tr -s ' '| cut -d' ' -f3)
-mvn clean package -f ./pom.xml
+mvn clean package spring-boot:repackage  -f ./pom.xml
 docker build -t travelapp/sbgatewaysv:latest ./travel-gateway
 docker build -t travelapp/sbflightsv:latest ./flight-booking-service
 docker build -t travelapp/sbhotelsv:latest ./hotel-booking-service
