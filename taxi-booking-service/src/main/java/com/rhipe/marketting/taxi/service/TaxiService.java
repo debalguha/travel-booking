b1be3b8b8408aecd00366a6099257937d9b08a9f
debalguha <debalguha.active@gmail.com>
2023-03-28 12:40:46 +1100

    Taxi API fix

--- taxi-booking-service/src/main/java/com/rhipe/marketting/taxi/service/TaxiService.java
+++ taxi-booking-service/src/main/java/com/rhipe/marketting/taxi/service/TaxiService.java
@@ -2,6 +2,7 @@ package com.rhipe.marketting.taxi.service;
 
 import com.rhipe.marketting.taxi.model.Taxi;
 import com.rhipe.marketting.taxi.repository.TaxiRepository;
+import com.rhipe.travel.booking.dto.TaxiResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,10 @@ import java.util.Optional;
 public class TaxiService {
     final TaxiRepository taxiRepository;
 
-    public Optional<Taxi> lookupTaxi(long taxiId) {
-        return taxiRepository.findById(taxiId);
+    public TaxiResponseDTO lookupTaxi(String taxiId) {
+        return taxiRepository.findTaxiByTaxiId(taxiId)
+                .map(t -> new TaxiResponseDTO(t.getId(), t.getTaxiId()))
+                .orElseThrow(() -> new RuntimeException("Unable to locate taxi by %s".formatted(taxiId)));
     }
     public List<String> allTaxiNums() {
         return taxiRepository.allTaxiNums();
